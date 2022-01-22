2a9f8a779959ca9ebf078b2ec0d9a8b3671e929a
Anjani <pranav.ch.12@outlook.com>
2022-01-22 10:51:56 +0100

    Updates

--- js/singleCountry.js
+++ js/singleCountry.js
@@ -1,54 +1,61 @@
-const countryFlag = document.querySelector('#country-flag')
-const countryName = document.querySelector('#country-name')
-const list1 = document.querySelector('#list-1')
-const list2 = document.querySelector('#list-2')
-const borderCountriesContainer = document.querySelector('#border-countries div')
+const countryFlag = document.querySelector('#country-flag');
+const countryName = document.querySelector('#country-name');
+const list1 = document.querySelector('#list-1');
+const list2 = document.querySelector('#list-2');
+const borderCountriesContainer = document.querySelector('#border-countries div');
 
-fetch('http://restcountries.eu/rest/v2/callingcode/' + sessionStorage.getItem('callingCode'))
-    .then(res => res.json())
-    .then(data => {
-        let country = data[0]
-        let currencies = ''
-        let languages = ''
-        let borderCountries = []
+window.addEventListener('DOMContentLoaded', async () => {
+    const code = sessionStorage.getItem('callingCode');
 
-        country.borders.forEach(border => borderCountries.push(fetch('https://restcountries.eu/rest/v2/alpha?codes=' + border)
+    const res = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+    const data = await res.json();
+
+    const country = data[0];
+    const countryCurrencies = country.currencies;
+    const countryLanguages = country.languages;
+    const borderCountries = [];
+
+    let currencies = '';
+    let languages = '';
+
+    console.log(country)
+
+    if (country.borders) {
+        country.borders.forEach(borderCountryCode => borderCountries.push(fetch('https://restcountries.com/v3.1/alpha/' + borderCountryCode)
             .then(res => res.json())
-            .then(data => data)))
+            .then(data => data)));
+    }
 
-        country.currencies.forEach(currency => currencies += currency.code + ', ')
-        country.languages.forEach(language => languages += language.name + ', ')
+    for (const currency in countryCurrencies) currencies += country.currencies[currency].name + ', ';
+    for (const language in countryLanguages) languages += countryLanguages[language] + ', ';
 
-        countryFlag.src = country.flag
-        countryName.innerText = country.name
+    countryFlag.src = country.flags.png;
+    countryName.innerText = country.name.common;
 
-        list1.innerHTML = `
-            <li>Native Name: <span>${country.nativeName}</span></li>
-            <li>Population: <span>${country.population}</span></li>
-            <li>Region: <span>${country.region}</span></li>
-            <li>Sub Region: <span>${country.subregion}</span></li>
-            <li>Capital: <span>${country.capital}</span></li>
-        `
+    list1.innerHTML = `
+        <li>Population: <span>${country.population}</span></li>
+        <li>Region: <span>${country.region}</span></li>
+        <li>Sub Region: <span>${country.subregion}</span></li>
+        <li>Capital: <span>${country.capital[0]}</span></li>
+    `;
 
-        list2.innerHTML = `
-            <li>Top Level Domain: <span>${country.topLevelDomain[0]}</span></li>
-            <li>Currencies: <span>${currencies}</span></li>
-            <li>Languages: <span>${languages}</span></li>
-        `
+    list2.innerHTML = `
+        <li>Currencies: <span>${currencies}</span></li>
+        <li>Languages: <span>${languages}</span></li>
+    `;
 
+    if (country.borders) {
         Promise.all(borderCountries)
-            .then(countries => {
-                countries.forEach(country => {
-                    const div = document.createElement('div')
+        .then(countries => {
+            countries.forEach(country => {
+                const div = document.createElement('div');
 
-                    div.className = 'border-country shadow'
-                    div.innerText = country[0].name
+                div.className = 'border-country shadow';
+                div.innerText = country[0].name.common;
 
-                    borderCountriesContainer.appendChild(div)
-                })
+                borderCountriesContainer.appendChild(div);
             })
-            .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
-
-// http://restcountries.eu/rest/v2/callingcode/{callingCode}
+        })
+        .catch(err => console.log(err));
+    } else borderCountriesContainer.parentElement.style.display = 'none';
+});
--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,41 +1,43 @@
-const themeStyles = document.querySelectorAll('#theme-styles')[0]
-const toggleTheme = document.querySelector('#toggle-theme')
-const toggleIcon = document.querySelector('#toggle-icon')
-const themeMode = document.querySelector('#theme-mode')
+const themeStyles = document.querySelectorAll('#theme-styles')[0];
+const toggleTheme = document.querySelector('#toggle-theme');
+const toggleIcon = document.querySelector('#toggle-icon');
+const themeMode = document.querySelector('#theme-mode');
 
-let style = ''
+let style = '';
 
 const changeStylesheet = theme => {
-    if (theme === 'dark') themeStyles.href.replace('light', 'dark')
-    else if (theme === 'light') themeStyles.href.replace('dark', 'light')
-}
+    if (theme === 'dark') themeStyles.href.replace('light', 'dark');
+    else if (theme === 'light') themeStyles.href.replace('dark', 'light');
+};
 
 const enableLightTheme = () => {
-    themeStyles.href = themeStyles.href.replace('dark', 'light')
-    toggleIcon.name = 'moon-outline'
-    themeMode.innerText = 'Dark Mode'
-    style = 'light'
-}
+    themeStyles.href = themeStyles.href.replace('dark', 'light');
+    toggleIcon.name = 'sunny-outline';
+    themeMode.innerText = 'Light Mode';
+    style = 'light';
+};
 
 const enableDarkTheme = () => {
-    themeStyles.href = themeStyles.href.replace('light', 'dark')
-    toggleIcon.name = 'sunny-outline'
-    themeMode.innerText = 'Light Mode'
-    style = 'dark'
-}
-
-if (localStorage.getItem('styleTheme')) {
-    if (localStorage.getItem('styleTheme') === 'light') enableLightTheme()
-    else enableDarkTheme()
-}
+    themeStyles.href = themeStyles.href.replace('light', 'dark');
+    toggleIcon.name = 'moon-outline';
+    themeMode.innerText = 'Dark Mode';
+    style = 'dark';
+};
 
 toggleTheme.addEventListener('click', e => {
     if (e.target.id === 'toggle-theme' || e.target.parentElement.id === 'toggle-theme') {
-        if (themeStyles.href.includes('light')) enableDarkTheme()
-        else enableLightTheme()
+        if (themeStyles.href.includes('light')) enableDarkTheme();
+        else enableLightTheme();
 
-        changeStylesheet(style)
+        changeStylesheet(style);
 
-        localStorage.setItem('styleTheme', style)
+        localStorage.setItem('styleTheme', style);
     }
-})
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('styleTheme')) {
+        if (localStorage.getItem('styleTheme') === 'light') enableLightTheme()
+        else enableDarkTheme()
+    }
+});
